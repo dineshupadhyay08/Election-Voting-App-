a452fc89cc5b3d3fe7f4b2fd47c7de926aa9f223
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-07-13 23:30:35 +0530

    refactor: update application entry point

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
-import router from "./routes/index.jsx";
 import { ToastContainer } from "react-toastify";
+import router from "./routes/index.jsx";
+import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import "./index.css";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    <ToastContainer />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <AuthProvider>
+          <RouterProvider router={router} />
+          <ToastContainer position="top-right" theme="dark" />
+        </AuthProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
