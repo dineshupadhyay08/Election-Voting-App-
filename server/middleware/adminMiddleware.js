5d23f33a7d83ea8eacb945390296fc58b69a4fa6
dineshupadhyay08 <dineshupadhyay666@gmail.com>
2026-09-12 22:33:59 +0530

    fix: strengthen admin authorization

--- server/middleware/adminMiddleware.js
+++ server/middleware/adminMiddleware.js
@@ -1,22 +1,10 @@
 const HttpError = require("./HttpError");
-const VoterModel = require("../model/voterModel");
 
-const adminMiddleware = async (req, res, next) => {
-  try {
-    if (!req.user || !req.user.id) {
-      return next(new HttpError("Not authenticated", 401));
-    }
-
-    const voter = await VoterModel.findById(req.user.id);
-
-    if (!voter || voter.isAdmin !== true) {
-      return next(new HttpError("Admin access only", 403));
-    }
-
-    // ✅ trusted admin
+const adminMiddleware = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
     next();
-  } catch (err) {
-    return next(new HttpError("Admin authorization failed", 500));
+  } else {
+    return next(new HttpError("Admin access required.", 403));
   }
 };
 
